b624e10a7ed8c0665a4b6886680ae495174e825b
Kamal <99723884+kamal-kumar-001@users.noreply.github.com>
2023-04-10 13:45:49 +0530

    update admin page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Admin from '../../components/Admin';
 import { hasToken } from '../../middleware/checkUser'
-
+import connectDb from '../../middleware/mongo';
+import Quiz from '../../Models/Quiz';
 
 export default function AdminPage({ quizzes}) {
+  // console.log(quizzes);
   return <Admin quizzes={quizzes} />;
 }
 
-
-
 export async function getServerSideProps(context) {
+  
   const { req } = context
-  const headers = req.headers;
   const token = await hasToken(context.req)
   if(!token){
       return {
@@ -25,13 +25,18 @@ export async function getServerSideProps(context) {
   // const response = await fetch(`${apiUrl}/api/quiz`, {
   //   headers: headers,
   // });
-  const response = await fetch(`https://quiz-mrnormal128-gmailcom.vercel.app/api/quiz`, {
-    headers: headers,
+  // const response = await fetch(`https://quiz-mrnormal128-gmailcom.vercel.app/api/quiz`, {
+  //   headers: headers,
+  // });
+  const userId = token.user._id
+  
+  await connectDb();
+  const quizzes = await Quiz.find({ user: userId }).sort({
+    createdAt: -1,
   });
-  const data = await response.json();
   return {
     props: {
-      quizzes: data?.quizzes || null,
+      quizzes: JSON.parse(JSON.stringify(quizzes)),
     },
   };
 }
